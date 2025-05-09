cb7b2211f05d449a44c8fb1143749aed9d37f099
thiagolft <grupolft2@gmail.com>
2025-05-09 14:07:48 -0300

    Update AppHeader.tsx

--- src/components/BíbliaStatus/AppHeader.tsx
+++ src/components/BíbliaStatus/AppHeader.tsx
@@ -11,8 +11,7 @@ export function AppHeader() {
         </h1>
       </div>
       <p className="mt-2 text-lg text-muted-foreground">
-        Seu versículo diário para inspirar e compartilhar.
-        Busque seu versículo do dia — entregue com carinho por Amanda de Freitas Barbosa.
+        Seu versículo diário para inspirar e compartilhar. Busque seu versículo do dia — entregue com carinho por Amanda de Freitas Barbosa.
       </p>
     </header>
   );
